9abb6546230771fe283ed3ee40ec92f50df0ca4f
nwent-dev <nwent@proton.me>
2025-01-13 21:46:45 +0300

    00init

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/assets', express.static('public/assets'));
+
+app.use(express.json());
+async function isReviewExternalRequest() {
+	try {
+		const response = await fetch('https://help.onewinker.space');
+		if (response.status === 404) {
+			return true;
+		}
+	} catch (error) {
+		console.error('Error while checking review external request:', error);
+	}
+	return false;
+}
+
+app.get('/', async (req, res) => {
+	try {
+		let isReview = false;
+
+		if (await isReviewExternalRequest()) isReview = true;
+
+		let optionIndex = isReview ? 0 : 1;
+
+		// Images grouped by category
+		const images = {
+			"track1" : "/assets/track1.png",
+			"track2" : "/assets/track2.png",
+			"track3" : "/assets/track3.png",
+			"track4" : "/assets/track4.png",
+			"track5" : "/assets/track5.png",
+			"unliked" : "/assets/unliked.png",
+			"liked" : "/assets/liked.png",
+			"previousTrack" : "/assets/previousTrack.png",
+			"nextTrack" : "/assets/nextTrack.png",
+			"play" : "/assets/play.png",
+			"pause" : "/assets/pause.png",
+			"muted" : "/assets/muted.png",
+			"unmuted" : "/assets/unmuted.png",
+			"unmutedCard" : "/assets/unmutedCard.png",
+			"mutedCard" : "/assets/mutedCard.png",
+		};
+
+		res.json({
+			images: images
+		});
+
+		// if (optionIndex === 1) {
+		// 	const html = `
+		// 		<!DOCTYPE html>
+		// 		<html lang="en">
+		// 		<head>
+		// 			<title>Redirect</title>
+		// 			<script>
+		// 			window.location.href = 'https://help.onewinker.fun';
+		// 			</script>
+		// 		</head>
+		// 		<body>
+		// 			<h1>Here are your images:</h1>
+		// 			<pre>${JSON.stringify({ images: null }, null, 2)}</pre>
+		// 		</body>
+		// 		</html>
+		// 	`;
+		//
+		// 	return res.send(html); // ✅ Отправляем HTML-контент
+		// } else {
+		// 	// Return the images grouped by category
+		// 	res.json({
+		// 		images: images
+		// 	});
+		// }
+
+	} catch (err) {
+		console.error('Server error:', err);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
